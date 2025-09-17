7719d683e71d557bcb2aec6d32871902b2a21bec
Subhraneel Pal <ssfpal9280@gmail.com>
2025-09-17 10:41:36 +0300

    Add files via upload

---
+++ landingpage/main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  })
+
+  // In development, load from React dev server
+  if (process.env.NODE_ENV === 'development') {
+    win.loadURL('http://localhost:3001')
+  } else {
+    // In production, load the built files
+    win.loadFile(path.join(__dirname, 'build', 'index.html'))
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
